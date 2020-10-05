abbeba157aed9a4fd1c0257f779f466008b0a007
Lasisi <56618398+kenny15335@users.noreply.github.com>
2020-10-05 23:07:59 +0100

    Updates

---
+++ precache-manifest.a2e492b1b68ed54d57aebc65613b6bbb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2053ed9b26cc99a68672d195920a6afb",
+    "url": "/onjuno/index.html"
+  },
+  {
+    "revision": "fce8c1e11798b1f27664",
+    "url": "/onjuno/static/css/main.60f57208.chunk.css"
+  },
+  {
+    "revision": "db47d2cb086700efc388",
+    "url": "/onjuno/static/js/2.4bd741ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/onjuno/static/js/2.4bd741ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce8c1e11798b1f27664",
+    "url": "/onjuno/static/js/main.9f9df056.chunk.js"
+  },
+  {
+    "revision": "af48bf90289d9558d2a8",
+    "url": "/onjuno/static/js/runtime-main.deae89bd.js"
+  },
+  {
+    "revision": "3498f266deac7ec22123393c02c0cca4",
+    "url": "/onjuno/static/media/animation_640_ke30n3vw.3498f266.gif"
+  },
+  {
+    "revision": "3d22fca12983715d3b0542c47fb4b56b",
+    "url": "/onjuno/static/media/images.3d22fca1.jpg"
+  },
+  {
+    "revision": "4f0469b33aecbf946708beb8f47101b5",
+    "url": "/onjuno/static/media/side.4f0469b3.png"
+  },
+  {
+    "revision": "1e182430119c57525492dc3bd3be221b",
+    "url": "/onjuno/static/media/usa.1e182430.png"
+  }
+]);
